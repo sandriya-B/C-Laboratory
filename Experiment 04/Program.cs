11b87b9c3738895555e7767cb63f0c8bc6e777ed
sandriya-B <sandriyabalu@gmail.com>
2026-09-17 12:23:21 +0530

    Add files via upload

---
+++ Experiment 04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Button
+{
+    // Delegate
+    public delegate void ButtonClickHandler();
+
+    // Event
+    public event ButtonClickHandler? ButtonClicked;
+
+    // Method to raise the event
+    public void Click()
+    {
+        Console.WriteLine("Button is clicked!");
+
+        ButtonClicked?.Invoke();
+    }
+}
+
+class Program
+{
+    // Event handler method
+    static void ShowMessage()
+    {
+        Console.WriteLine("Hello! Button click event handled.");
+    }
+
+    static void Main()
+    {
+        Button btn = new Button();
+
+        // Connect the method to the event
+        btn.ButtonClicked += ShowMessage;
+
+        // Simulate button click
+        btn.Click();
+
+        Console.ReadLine();
+    }
+}
